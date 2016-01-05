c3aa9f35b033af55ba3db803ab99e01a179af22c
n8joseph <natejoseph@Nates-MBP.home>
2016-01-04 22:41:04 -0500

    commit 2

---
+++ js/app.js
@@ -0,0 +1,250 @@
+angular.module('Vidmix', ['ui.router', 'vmLibrary'])
+	
+
+
+	.controller('MainCtrl', ['$scope', 'VidLength', '$http', '$log', 'VideosService', 'API_KEY', function($scope, VidLength, $http, $log, VideosService, API_KEY) {
+
+       var idArray = ['A3ckIovZRwk', 'yOom_hezEKI', 'PAgY5MqpUq4']
+       var myid = 'A3ckIovZRwk';
+       var lengthArray = [];
+       var clipHeightOffsetArray = [];
+       var vidDB = [];
+       var maxTotalWidth = 0;
+       $scope.maxTotalWidth = 0;
+       
+       
+
+		// angular.element(document).ready(function (data) {
+		// 	function makeHandle(i) {
+		// 		return function(api) {
+		// 			console.log("api.duration " + i + " is " + api.duration);
+		// 	    	lengthArray[i] = api.duration / 2;
+		// 	    	console.log("lengthArray " + i + " is " + lengthArray[i]);
+		// 		}
+		// 	};
+		// 	for (i = 0; i < idArray.length; i++) {
+		// 		VidLength.fetchURL(idArray[i]).then(makeHandle(i));
+		// 	}
+
+		// })
+
+
+
+		for (i = 0; i < idArray.length; i++) {
+			console.log("COUNT ME");
+			clipHeightOffsetArray.push(40 * i)
+		}
+		
+		console.log("lengthArray: ... " + lengthArray)
+
+		// for (i = 0; i < idArray.length; i++) {
+		// 	vidDB.push({
+		// 		vidId: idArray[i],
+		// 		vidLength: lengthArray[i],
+		// 		clipHeightOffset: clipHeightOffsetArray[i]
+		// 	})
+		// }
+		
+		// console.log("vidDB[1].vidId: ... " + vidDB[1].vidId)
+		// console.log("vidDB[1].vidLength: ... " + vidDB[1].vidLength)
+		// console.log("vidDB[1].clipHeightOffset: ... " + vidDB[1].clipHeightOffset)
+
+		var setMaxTotalWidth = function() {
+			for (i = 0; i < lengthArray.length; i++) {
+				maxTotalWidth += lengthArray[i];
+			}
+			console.log("setMaxTotalWidth ran successfully")
+			$scope.maxTotalWidth = maxTotalWidth
+		}	
+
+		// var showContainer = function() {
+		// 	$scope.showContainer = "block";
+		// }
+
+		
+
+		var createGrid = function() {
+			console.log("lengthArray: " + lengthArray)
+			console.log("idArray.length: " + idArray.length)
+
+			for (i = 0; i < idArray.length; i++) {
+				vidDB.push({
+					vidId: idArray[i],
+					vidLength: lengthArray[i],
+					clipHeightOffset: clipHeightOffsetArray[i]
+				})
+				console.log("idarray: " + idArray)
+				console.log("lengtharray: " + lengthArray)
+				console.log("vidDB: " + vidDB)
+			}
+			$scope.vidDB = vidDB;
+
+			setMaxTotalWidth();
+			// showContainer();
+
+
+		}
+
+		$scope.createGrid = createGrid;
+
+		$scope.clipHeightOffsetArray = clipHeightOffsetArray;
+		$scope.lengthArray = lengthArray;
+		$scope.idArray = idArray;
+		$scope.youtubeControl = {};
+		$scope.vidDB = [];
+		$scope.showContainer = "none";
+
+		
+		
+
+
+		
+
+		var addLengths = function(data) {
+					
+			function makeHandle(i) {
+				return function(api) {
+					console.log("api.duration " + i + " is " + api.duration);
+			    	lengthArray[i] = api.duration / 2;
+			    	console.log("lengthArray " + i + " is " + lengthArray[i]);
+			    	console.log(lengthArray)
+				}
+			};
+			for (i = 0; i < idArray.length; i++) {
+				VidLength.fetchURL(idArray[i]).then(makeHandle(i));
+			}
+
+		}
+
+		$scope.tester2 = function() {
+			var myPromise = function() {
+				var deferred = $q.defer();
+				promise.then(function(res) {
+					deferred.resolve
+				})
+			}
+			
+
+
+
+			idArray.push('IQBC5URoF0s')
+			addLengths();
+			// createGrid();
+
+		}
+
+
+
+
+
+		$scope.dragOptions = {
+        start: function(e) {
+          console.log("STARTING");
+        },
+        drag: function(e) {
+          console.log("DRAGGING");
+        },
+        stop: function(e) {
+          console.log("STOPPING");
+        },
+        container: 'container'
+    }
+
+    init();
+
+    function init() {
+      $scope.youtube = VideosService.getYoutube();
+      $scope.results = VideosService.getResults();
+      $scope.upcoming = VideosService.getUpcoming();
+      $scope.history = VideosService.getHistory();
+      $scope.playlist = true;
+    }
+
+    $scope.launch = function (id, title) {
+      VideosService.launchPlayer(id, title);
+      VideosService.archiveVideo(id, title);
+      VideosService.deleteVideo($scope.upcoming, id);
+      $log.info('Launched id:' + id + ' and title:' + title);
+    };
+
+    $scope.queue = function (id, title) {
+      idArray.push(id);
+      $log.info('Queued id:' + id + ' and title:' + title);
+    };
+
+    $scope.delete = function (list, id) {
+      VideosService.deleteVideo(list, id);
+    };
+
+    $scope.search = function () {
+      $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: 'AIzaSyCCjazRr2tqfJSotjepk6NMj7KcrzRuySU',
+          type: 'video',
+          maxResults: '8',
+          part: 'id,snippet',
+          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+          q: this.query
+        }
+      })
+      .success( function (data) {
+        VideosService.listResults(data);
+        $log.info(data);
+      })
+      .error( function () {
+        $log.info('Search error');
+      });
+    }
+
+    $scope.tabulate = function (state) {
+      $scope.playlist = state;
+    }
+
+
+document.addEventListener( "DOMContentLoaded", function() {
+ 
+          var popcorn = Popcorn.smart( "#youtube" );
+
+          popcorn.footnote({
+            start: 2,
+            end: 5,
+            target: "footnote",
+             text: "Pop!"
+           });
+ 
+         }, false );
+
+    }])
+
+	.controller('VidCtrl', ['$scope', function($scope) {
+
+	 
+
+	}])
+
+
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'templates/home.html',
+				controller: 'MainCtrl',
+				// resolve: {
+				// 	geoname: ['$stateParams', 'getGeoname', function($stateParams, getGeoname) {
+				// 		console.log('real getGeoname is called');
+				// 		return getGeoname($stateParams.country);
+				// 	}]
+				// }
+			})
+			.state('vidz', {
+				url: '/vidz',
+				templateUrl: 'templates/vidz.html',
+				controller: 'VidCtrl'	
+			})
+	}])
+
+	
+
+
---
+++ js/library.js
@@ -0,0 +1,441 @@
+angular.module('vmLibrary', [])
+
+	.constant('API_KEY', 'AIzaSyCCjazRr2tqfJSotjepk6NMj7KcrzRuySU')
+	.constant('YT_event', {
+							  STOP:            0, 
+							  PLAY:            1,
+							  PAUSE:           2,
+							  STATUS_CHANGE:   3 })
+	.constant('idArray', ['A3ckIovZRwk', 'yOom_hezEKI', 'PAgY5MqpUq4'])
+	
+
+	.factory('VidLength', ['$http', 'API_KEY', function($http, API_KEY) {
+
+		console.log('hi factory ( page load )');
+
+		  return { 
+		    fetchURL:
+			 function(myid) { console.log('factory fired')
+			   return $http({
+			     method: 'GET',
+			     url: 'https://www.googleapis.com/youtube/v3/videos',
+			     cache: true,
+			     params: {
+					part: "contentDetails",
+					id : myid,
+					key: API_KEY
+				 }
+			    })
+			    .then(function(response) { console.log('API call worked');
+		          rawDuration = response.data.items[0].contentDetails.duration.split("S")[0].split("M")
+		          var minutes = rawDuration[0].split("PT")[1]
+		          var seconds = rawDuration[1]
+		          var duration = (Number(minutes) * 60) + Number(seconds)
+		          console.log("video duration is " + duration + " seconds")
+		          return { data:
+		          	duration, duration
+		          }
+		        }, function(response) {
+		          console.log("failure")   
+		  	})
+		   }
+
+	}
+	}])
+
+
+	.service('VideosService', ['$window', '$rootScope', '$log', function ($window, $rootScope, $log) {
+
+	  var service = this;
+
+	  var youtube = {
+	    ready: false,
+	    player: null,
+	    playerId: null,
+	    videoId: null,
+	    videoTitle: null,
+	    playerHeight: '480',
+	    playerWidth: '640',
+	    state: 'stopped'
+	  };
+	  var results = [];
+	  var upcoming = [
+	    {id: 'kRJuY6ZDLPo', title: 'La Roux - In for the Kill (Twelves Remix)'},
+	    {id: '45YSGFctLws', title: 'Shout Out Louds - Illusions'},
+	    {id: 'ktoaj1IpTbw', title: 'CHVRCHES - Gun'},
+	    {id: '8Zh0tY2NfLs', title: 'N.E.R.D. ft. Nelly Furtado - Hot N\' Fun (Boys Noize Remix) HQ'},
+	    {id: 'zwJPcRtbzDk', title: 'Daft Punk - Human After All (SebastiAn Remix)'},
+	    {id: 'sEwM6ERq0gc', title: 'HAIM - Forever (Official Music Video)'},
+	    {id: 'fTK4XTvZWmk', title: 'Housse De Racket â˜â˜€â˜ Apocalypso'}
+	  ];
+	  var history = [
+	    {id: 'XKa7Ywiv734', title: '[OFFICIAL HD] Daft Punk - Give Life Back To Music (feat. Nile Rodgers)'}
+	  ];
+
+	  $window.onYouTubeIframeAPIReady = function () {
+	    $log.info('Youtube API is ready');
+	    youtube.ready = true;
+	    service.bindPlayer('placeholder');
+	    service.loadPlayer();
+	    $rootScope.$apply();
+	  };
+
+	  function onYoutubeReady (event) {
+	    $log.info('YouTube Player is ready');
+	    youtube.player.cueVideoById(history[0].id);
+	    youtube.videoId = history[0].id;
+	    youtube.videoTitle = history[0].title;
+	  }
+
+	  function onYoutubeStateChange (event) {
+	    if (event.data == YT.PlayerState.PLAYING) {
+	      youtube.state = 'playing';
+	    } else if (event.data == YT.PlayerState.PAUSED) {
+	      youtube.state = 'paused';
+	    } else if (event.data == YT.PlayerState.ENDED) {
+	      youtube.state = 'ended';
+	      service.launchPlayer(upcoming[0].id, upcoming[0].title);
+	      service.archiveVideo(upcoming[0].id, upcoming[0].title);
+	      service.deleteVideo(upcoming, upcoming[0].id);
+	    }
+	    $rootScope.$apply();
+	  }
+
+	  this.bindPlayer = function (elementId) {
+	    $log.info('Binding to ' + elementId);
+	    youtube.playerId = elementId;
+	  };
+
+	  this.createPlayer = function () {
+	    $log.info('Creating a new Youtube player for DOM id ' + youtube.playerId + ' and video ' + youtube.videoId);
+	    return new YT.Player(youtube.playerId, {
+	      height: youtube.playerHeight,
+	      width: youtube.playerWidth,
+	      playerVars: {
+	        rel: 0,
+	        showinfo: 0
+	      },
+	      events: {
+	        'onReady': onYoutubeReady,
+	        'onStateChange': onYoutubeStateChange
+	      }
+	    });
+	  };
+
+	  this.loadPlayer = function () {
+	    if (youtube.ready && youtube.playerId) {
+	      if (youtube.player) {
+	        youtube.player.destroy();
+	      }
+	      youtube.player = service.createPlayer();
+	    }
+	  };
+
+	  this.launchPlayer = function (id, title) {
+	    youtube.player.loadVideoById(id);
+	    youtube.videoId = id;
+	    youtube.videoTitle = title;
+	    return youtube;
+	  }
+
+	  this.listResults = function (data) {
+	    results.length = 0;
+	    for (var i = data.items.length - 1; i >= 0; i--) {
+	      results.push({
+	        id: data.items[i].id.videoId,
+	        title: data.items[i].snippet.title,
+	        description: data.items[i].snippet.description,
+	        thumbnail: data.items[i].snippet.thumbnails.default.url,
+	        author: data.items[i].snippet.channelTitle
+	      });
+	    }
+	    return results;
+	  }
+
+	  this.queueVideo = function (id, title) {
+	    upcoming.push({
+	      id: id,
+	      title: title
+	    });
+	    return upcoming;
+	  };
+
+	  this.archiveVideo = function (id, title) {
+	    history.unshift({
+	      id: id,
+	      title: title
+	    });
+	    return history;
+	  };
+
+	  this.deleteVideo = function (list, id) {
+	    for (var i = list.length - 1; i >= 0; i--) {
+	      if (list[i].id === id) {
+	        list.splice(i, 1);
+	        break;
+	      }
+	    }
+	  };
+
+	  this.getYoutube = function () {
+	    return youtube;
+	  };
+
+	  this.getResults = function () {
+	    return results;
+	  };
+
+	  this.getUpcoming = function () {
+	    return upcoming;
+	  };
+
+	  this.getHistory = function () {
+	    return history;
+	  };
+
+	}])
+
+	.factory("youTubeApiService", function($q, $window) {
+	  
+	  var deferred = $q.defer();
+	  var apiReady = deferred.promise;
+
+	  $window.onYouTubeIframeAPIReady = function() {
+	    deferred.resolve();
+	  }
+
+	  return {
+	    onReady: function(callback) {
+	      apiReady.then(callback);
+	    }
+	  }   
+
+	})
+	
+	.directive('ngDraggable', ['$document', 'VidLength', function($document, VidLength) {
+	  return {
+	    restrict: 'A',
+	    scope: {
+	      dragOptions: '=ngDraggable'
+	    },
+	    link: function(scope, elem, attr) {
+	      var startX, startY, x = 0, y = 0,
+	          start, stop, drag, container;
+
+	      var width  = elem[0].offsetWidth,
+	          height = elem[0].offsetHeight;
+
+	      // Obtain drag options
+	      if (scope.dragOptions) {
+	        start  = scope.dragOptions.start;
+	        drag   = scope.dragOptions.drag;
+	        stop   = scope.dragOptions.stop;
+	        var id = scope.dragOptions.container;
+	        if (id) {
+	            container = document.getElementById(id).getBoundingClientRect();
+	        }
+	      }
+
+
+	      // Bind mousedown event
+	      elem.on('mousedown', function(e) {
+	        e.preventDefault();
+	        console.log(e)
+	        startX = e.clientX - elem[0].offsetLeft;
+	      //  startY = e.clientY - elem[0].offsetTop;
+	        $document.on('mousemove', mousemove);
+	        $document.on('mouseup', mouseup);
+	        if (start) start(e);
+	      });
+
+	      // Handle drag event
+	      function mousemove(e) {
+	      //  y = e.clientY - startY;
+	        x = e.clientX - startX;
+	        setPosition();
+	        if (drag) drag(e);
+	      }
+
+	      // Unbind drag events
+	      function mouseup(e) {
+	        $document.unbind('mousemove', mousemove);
+	        $document.unbind('mouseup', mouseup);
+	        if (stop) stop(e);
+	      }
+
+	      // Move element, within container if provided
+	      function setPosition() {
+	        if (container) {
+	          if (x < container.left) {
+	            x = container.left - 8;
+	          } else if (x > container.right - width) {
+	            x = container.right - width - 8;
+	          }
+	        //   if (y < container.top) {
+	        //     y = container.top;
+	        //   } else if (y > container.bottom - height) {
+	        //     y = container.bottom - height;
+	        //   }
+	         }
+
+	        elem.css({
+	       //   top: y + 'px',
+	          left:  x + 'px'
+	        });
+	      }
+	    }
+	  }
+
+	}])
+	
+	.directive('ngVidUnit', [function() {
+		return {
+			restrict: 'E',
+			template: '<div></div>',
+			link: function(scope, element) {
+				// angular.element(document).ready(function () {
+    				
+    // 				scope.clipHeightOffset = [];
+
+    // 				for (i = 0; i < scope.idArray.length; i++) {
+    // 					console.log("COUNT ME");
+    // 					scope.clipHeightOffset.push(40 * i)
+    // 				}
+    // 				console.log(scope.clipHeightOffset)
+
+    // 			});
+			}
+
+		}
+	}])
+
+	.directive('youtube', function($window, YT_event, youTubeApiService) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "@"
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element, attrs, $rootScope) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+      var player;
+
+      youTubeApiService.onReady(function() {
+        player = setupPlayer(scope, element);
+      });
+
+      function setupPlayer(scope, element) {
+        return new YT.Player(element.children()[0], {
+          playerVars: {
+            autoplay: 0,
+            html5: 1,
+            theme: "dark",
+            modestbranding: 1,
+            iv_load_policy: 3,
+            showinfo: 0,
+            controls: 0,
+            autohide: 1,
+            disablekb: 1,
+            rel: 0
+          },
+          
+          height: scope.height,
+          width: scope.width,
+          videoId: scope.videoid, 
+
+          events: {
+            'onStateChange': function(event) {
+              
+              var message = {
+                event: YT_event.STATUS_CHANGE,
+                data: ""
+              };
+              
+              switch(event.data) {
+                case YT.PlayerState.PLAYING:
+                  message.data = "PLAYING";
+                  break;
+                case YT.PlayerState.ENDED:
+                  message.data = "ENDED";
+                  break;
+                case YT.PlayerState.UNSTARTED:
+                  message.data = "NOT PLAYING";
+                  break;
+                case YT.PlayerState.PAUSED:
+                  message.data = "PAUSED";
+                  break;
+              }
+
+              scope.$apply(function() {
+                scope.$emit(message.event, message.data);
+              });
+
+			    var time, rate, remainingTime;
+			    clearTimeout(stopPlayTimer);
+			    if (event.data == YT.PlayerState.PLAYING) {
+			      time = player.getCurrentTime();
+			      // Add .4 of a second to the time in case it's close to the current time
+			      // (The API kept returning ~9.7 when hitting play after stopping at 10s)
+			      if (time + .4 < stopPlayAt) {
+			        rate = player.getPlaybackRate();
+			        remainingTime = (stopPlayAt - time) / rate;
+			        stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
+			      }
+			    }
+
+			   
+
+            }
+          } 
+        });        
+      }
+
+      scope.$watch('height + width', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+    
+        player.setSize(scope.width, scope.height);
+      
+      });
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+        
+        player.cueVideoById(scope.videoid);
+      
+      });
+
+      scope.$on(YT_event.STOP, function () {
+        player.seekTo(0);
+        player.stopVideo();
+      });
+
+      scope.$on(YT_event.PLAY, function () {
+        console.log("RECEIVING");
+        player.playVideo();
+      }); 
+
+      scope.$on(YT_event.PAUSE, function () {
+        player.pauseVideo();
+      });  
+
+
+
+    }  
+  };
+})
+			
+
+	
